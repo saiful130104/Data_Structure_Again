aefec7f058c94c81d2ee8c0f77575295f8c6cafe
Saiful <isaiful.mail@gmail.com>
2021-03-30 15:38:45 +0600

    Optimized Bubble_sort Implementation

--- Sorting/Bubble_Sort.c
+++ Sorting/Bubble_Sort.c
@@ -9,12 +9,16 @@ int main(){
 
     // Bubble sorting implementation start
     for(i=0; i<len; i++){
+        int is_already_sorted = 1;
         for(j=0; j<len-i-1; j++)
             if(ara[j]>ara[j+1]){
                 temp = ara[j];
                 ara[j] = ara[j+1];
                 ara[j+1]=temp;
+                is_already_sorted = 0;
             }
+        if(is_already_sorted)
+            break;
     }
     // Bubble sorting implementation end
 
