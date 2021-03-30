6993f98159df5ef2f8f73ba24f6d28d7caa74d2d
Saiful <isaiful.mail@gmail.com>
2021-03-30 14:16:09 +0600

    Implementation of Selection Sort

--- Sorting/selection_sort.c
+++ Sorting/selection_sort.c
@@ -2,19 +2,19 @@
 
 int main(){
     int ara[] = {10,5,2,8,7};
-    int len = sizeof(ara)/sizeof(ara[0]);
+    int i,j,min_index,temp,len = sizeof(ara)/sizeof(ara[0]);
     printf("Before Sorting.\n");
-    for(int i=0; i<len; i++)
+    for(i=0; i<len; i++)
         printf("%d ",ara[i]);
 
     // selection sorting implementation start
-    for(int i=0; i<len-1; i++){
-        int min_index = i;
-        for(int j=i+1; j<len; j++)
+    for(i=0; i<len-1; i++){
+        min_index = i;
+        for(j=i+1; j<len; j++)
             if(ara[j]<ara[min_index])
                 min_index=j;
         if(min_index != i){
-            int temp = ara[i];
+            temp = ara[i];
             ara[i]=ara[min_index];
             ara[min_index]=temp;
         }
@@ -22,7 +22,7 @@ int main(){
     // Selection sorting implementation end
 
     printf("\n\nAfter sorting.\n");
-    for(int i=0; i<len; i++)
+    for(i=0; i<len; i++)
         printf("%d ",ara[i]);
     printf("\n");
 }
